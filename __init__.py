14d7bd6805fa98ef8c7174151a50265c072cf5b2
Archie S <me@platinumxy.com>
2025-10-21 01:29:05 +0100

    Added basic huristic for detecting ocaml binarys

--- __init__.py
+++ __init__.py
@@ -2,17 +2,22 @@ from binaryninja import *
 
 from .src import regs, utils, calling_convention
 
-
 PluginCommand.register(
     "OCaml Helper\\Update Young and Exception Regs",
     "Rename r14 and r15 vars to caml_exeption_ptr and caml_young_ptr respectively",
     regs.update_young_and_err_regs,
-    utils.arch_is_amd64
+    utils.arch_is_ocaml_amd64
 )
 
 PluginCommand.register(
     "OCaml Helper\\Convert to OCaml AMD64 Calling Convention",
     "Set the calling convention of functions without one to OCaml AMD64",
     calling_convention.convert_to_ocaml_call_amd64,
-    utils.arch_is_amd64
+    utils.arch_is_ocaml_amd64
 )
+
+PluginCommand.register(
+    "OCaml Helper\\Toggle\\Ignore caml/ocaml mentions when detecting OCaml bins",
+    "Ignore caml/ocaml mentions when detecting OCaml bins",
+    utils.toggle_ignore_caml_mentions
+)   
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,29 @@
 from binaryninja import *
 
-def arch_is_amd64(bv:BinaryView) -> bool:
-    return bv.arch and bv.arch.name == "x86_64"
 
+IGNORE_CAML_MENTIONS = False
+def toggle_ignore_caml_mentions(bv:BinaryView):
+    global IGNORE_CAML_MENTIONS
+    IGNORE_CAML_MENTIONS = not IGNORE_CAML_MENTIONS
+
+    # annoyingly you've got to force an update
+    bv.update_analysis_and_wait()
+
+def arch_is_ocaml_amd64(bv:BinaryView) -> bool:
+    if (not bv.arch) or bv.arch.name != "x86_64":
+        return False
+    
+    
+    if IGNORE_CAML_MENTIONS:
+        return True
+
+    # Check weve ocaml strings in rodata
+    caml_cnt = string_cnt_in_bin(bv, b"caml")
+    ocaml_cnt = string_cnt_in_bin(bv, b"ocaml")
+    return (caml_cnt + ocaml_cnt) > 5 # arbitrary threshold
+
+
+def string_cnt_in_bin(bv:BinaryView, string: bytes) -> int:
+    return sum(bv.read(segment.start, segment.length).count(string) for segment in bv.segments)
 
 logger = Logger(session_id=0, logger_name="OCaml Helper")
